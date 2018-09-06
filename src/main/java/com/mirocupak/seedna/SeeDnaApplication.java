097ad78c861feaad2db25770aa54e5710c5b5e85
Miro Cupak <mirocupak@gmail.com>
2018-09-06 18:24:49 -0400

    Add logging with Logback, fix #6

--- src/main/java/com/mirocupak/seedna/SeeDnaApplication.java
+++ src/main/java/com/mirocupak/seedna/SeeDnaApplication.java
@@ -1,5 +1,7 @@
 package com.mirocupak.seedna;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeeDnaApplication implements CommandLineRunner {
 
+    private Logger logger = LoggerFactory.getLogger(SeeDnaApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SeeDnaApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Application started...");
+        logger.debug("Application started...");
     }
 }
